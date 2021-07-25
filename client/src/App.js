2cec826c9c881ace9b8197454b5d98f657d92dab
lordkhan2 <70976558+lordkhan2@users.noreply.github.com>
2021-07-25 19:27:41 +0600

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/components/InfoBar/InfoBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import closeIcon from "../../icons/closeIcon";
+import onlineIcon from "../../icons/onlineIcon";
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img className="onlineIcon" src={onlineIcon} alt="Online " />
+        <h3>{room}</h3>
+      </div>
+      <div className="rightInnerContainer">
+        <a href="/">
+          <img src={closeIcon} alt="close " />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default InfoBar;
